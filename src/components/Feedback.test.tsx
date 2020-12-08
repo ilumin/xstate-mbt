b53a37b7019daea9bcf5e8abac1dab090d6f004b
ilumin <iolumin@gmail.com>
2020-12-08 09:10:10 +0700

    add xstate test

--- src/components/Feedback.test.tsx
+++ src/components/Feedback.test.tsx
@@ -1,8 +1,122 @@
 import React from 'react'
-import Feedback from './Feedback'
 import { render, fireEvent, cleanup } from '@testing-library/react'
 import { assert } from 'chai'
+import { Machine } from 'xstate'
+import { createModel } from '@xstate/test'
 
+import Feedback from './Feedback'
+
+describe(`feebkack component`, () => {
+  // feedback machine use in component
+  const feedbackMachine = Machine(
+    {
+      id: 'feedback',
+      initial: 'question',
+      states: {
+        question: {
+          on: {
+            CLICK_GOOD: 'thanks',
+            CLICK_BAD: 'form',
+            CLOSE: 'closed',
+            ESC: 'closed',
+          },
+          meta: {
+            test: ({ getByTestId }: any) =>
+              assert.ok(getByTestId('question-screen')),
+          },
+        },
+        form: {
+          on: {
+            SUBMIT: [
+              {
+                target: 'thanks',
+                cond: 'formValid',
+              },
+            ],
+            CLOSE: 'closed',
+            ESC: 'closed',
+          },
+          meta: {
+            test: ({ getByTestId }: any) =>
+              assert.ok(getByTestId('form-screen')),
+          },
+        },
+        thanks: {
+          on: {
+            CLOSE: 'closed',
+            ESC: 'closed',
+          },
+          meta: {
+            test: ({ getByTestId }: any) =>
+              assert.ok(getByTestId('thanks-screen')),
+          },
+        },
+        closed: {
+          type: 'final',
+          meta: {
+            test: ({ queryByTestId }: any) =>
+              assert.isNull(queryByTestId('thanks-screen')),
+          },
+        },
+      },
+    },
+    {
+      guards: {
+        formValid: (_: any, e: HTMLInputElement) => e.value.length > 0,
+      },
+    }
+  )
+
+  // test model from feedback machine
+  // defined event action by using `fireEvent`
+  const testModel = createModel(feedbackMachine, {
+    events: {
+      CLICK_GOOD: ({ getByText }: any) => {
+        fireEvent.click(getByText('Good'))
+      },
+      CLICK_BAD: ({ getByText }: any) => {
+        fireEvent.click(getByText('Bad'))
+      },
+      CLOSE: ({ getByTestId }: any) => {
+        fireEvent.click(getByTestId('close-button'))
+      },
+      ESC: ({ baseElement }: any) => {
+        fireEvent.keyDown(baseElement, { key: 'Escape' })
+      },
+      SUBMIT: {
+        exec: async ({ getByTestId }: any, event: any) => {
+          fireEvent.change(getByTestId('response-input'), {
+            target: { value: event.value },
+          })
+          fireEvent.click(getByTestId('submit-button'))
+        },
+        cases: [{ value: 'something' }, { value: '' }],
+      },
+    },
+  })
+
+  const testPlan = testModel.getSimplePathPlans()
+  // const testPlan = testModel.getShortestPathPlans()
+
+  testPlan.forEach((plan) => {
+    describe(plan.description, () => {
+      beforeEach(cleanup)
+
+      plan.paths.forEach((path) => {
+        it(path.description, () => {
+          const rendered = render(<Feedback />)
+          return path.test(rendered)
+        })
+      })
+    })
+  })
+
+  it('coverage', () => {
+    testModel.testCoverage()
+  })
+})
+
+/**
 describe('feedback app', () => {
   afterEach(cleanup)
 
@@ -58,3 +172,4 @@ describe('feedback app', () => {
     assert.ok(getByTestId('empty-screen'))
   })
 })
+*/
